a53fd99467d7d6852236b3a5f07cc4fd9a7bb5c0
Austin Bonander <austin.bonander@gmail.com>
2016-10-06 00:38:37 -0700

    WIP tree construction complete, traversal in progress

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,14 @@
-extern crate algo;
 extern crate rand;
-
-use algo::select;
 
 use rand::Rng;
 
+mod select;
+
+const NO_NODE: usize = ::std::usize::MAX;
+
 pub struct VpTree<T, Df> {
     nodes: Vec<Node>,
-    data: Vec<T>,
+    items: Vec<T>,
     dist_fn: Df,
 }
 
@@ -19,7 +20,7 @@ impl<T, Df> VpTree<T, Df> where Df: FnMut(&T, &T) -> u64 {
     pub fn from_vec(items: Vec<T>, dist_fn: Df) -> Self {
         let mut self_ = VpTree {
             nodes: Vec::with_capacity(items.len()),
-            data: items,
+            items: items,
             dist_fn: dist_fn,
         };
 
@@ -30,12 +31,24 @@ impl<T, Df> VpTree<T, Df> where Df: FnMut(&T, &T) -> u64 {
 
     fn full_rebuild(&mut self) {
         self.nodes.clear();
-        self.rebuild(0, self.items.len());
+
+        let len = self.items.len();
+        let nodes_cap = self.nodes.capacity();
+
+        if len > nodes_cap {
+            self.nodes.reserve(len - nodes_cap);
+        }
+
+        self.rebuild(NO_NODE, 0, len);
     }
 
     /// Rebuild the tree in [start, end)
-    fn rebuild(&mut self, start: usize, end: usize) {
-        if start == end - 1 { return; }
+    fn rebuild(&mut self, parent_idx: usize, start: usize, end: usize) -> usize {
+        if start + 1 == end { return NO_NODE; }
+
+        if start + 2 == end {
+            self.push_node(parent_idx, start, 0);
+        }
 
         let pivot_idx = rand::thread_rng().gen_range(start, end);
         self.items.swap(start, pivot_idx);
@@ -43,23 +56,117 @@ impl<T, Df> VpTree<T, Df> where Df: FnMut(&T, &T) -> u64 {
         let threshold = {
             let (pivot, items) = self.items.split_first_mut().unwrap();
 
+            // Without this reborrow, the closure will try to borrow all of `self`.
             let mut dist_fn = &mut self.dist_fn;
 
+            // This function will partition around the median element so we don't need
+            // as second partial-sort run
             let median_thresh_item = select::median_inplace_by(
                 items,
-                |left, right| dist_fn(&pivot, left).cmp(dist_fn(&pivot, right))
+                |left, right| dist_fn(pivot, left).cmp(&dist_fn(pivot, right))
             );
 
             dist_fn(pivot, median_thresh_item)
         };
 
+        let self_idx = self.push_node(parent_idx, start, threshold);
 
+        let left_idx = self.rebuild(self_idx, start + 1, pivot_idx + 1);
+        let right_idx = self.rebuild(self_idx, pivot_idx + 1, end);
+
+        self.nodes[self_idx].left = left_idx;
+        self.nodes[self_idx].right = right_idx;
+
+        self_idx
+    }
+
+    fn push_node(&mut self, idx: usize, parent_idx: usize, threshold: u64) -> usize {
+        let self_idx = self.nodes.len();
+
+        self.nodes.push(Node {
+            idx: idx,
+            parent: parent_idx,
+            left: NO_NODE,
+            right: NO_NODE,
+            threshold: threshold,
+        });
+
+        self_idx
+    }
+
+    /// Add `new_items` to the tree and rebuild it.
+    pub fn extend<I: IntoIterator<Item = T>>(&mut self, new_items: I) {
+        self.items.extend(new_items);
+        self.full_rebuild();
+    }
+
+    /// Apply a new distance function and rebuild the tree, returning the transformed type.
+    pub fn dist_fn<Df_>(self, dist_fn: Df_) -> VpTree<T, Df_>
+        where Df_: FnMut(&T, &T) -> u64 {
+        let mut self_ = VpTree {
+            nodes: self.nodes,
+            items: self.items,
+            dist_fn: dist_fn,
+        };
+
+        self_.full_rebuild();
+
+        self_
+    }
+
+    pub fn items(&self) -> &[T] {
+        &self.items
+    }
+
+    pub fn neighbors<'t, 'o>(&'t self, origin: &'o T, radius: u64) -> Neighbors<'t, 'o, T, Df>
+    where T: Eq {
+        Neighbors {
+            tree: self,
+            origin: origin,
+            current_node: if self.nodes.len() > 0 { 0 } else { NO_NODE },
+            radius: radius
+        }
     }
 }
 
 struct Node {
     idx: usize,
+    parent: usize,
     left: usize,
     right: usize,
     threshold: u64,
+}
+
+pub struct Neighbors<'t, 'o, T: 't + 'o, Df: 't> {
+    tree: &'t VpTree<T, Df>,
+    origin: &'o T,
+    current_node: usize,
+    radius: u64,
+}
+
+impl<'t, 'o, T: 't + 'o, Df: 't> Iterator for Neighbors<'t, 'o, T, Df>
+where Df: Fn(&T, &T) -> u64, T: Eq {
+    type Item = (&'t T, u64);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.current_node == NO_NODE {
+            return None;
+        }
+
+        if self.tree.nodes.len() == 0 { return None; }
+
+        let cur_node = &self.tree.nodes[self.current_node];
+
+        let dist_to_cur = (self.tree.dist_fn)(&self.origin, &self.tree.items[cur_node.idx]);
+
+        if dist_to_cur <= cur_node.threshold {
+            if cur_node.left == NO_NODE {
+
+            }
+        }
+
+        if  dist_to_cur <= self.radius {
+
+        }
+    }
 }
--- src/select.rs
+++ src/select.rs
@@ -4,6 +4,14 @@ use std::mem;
 // The threshold below which qselect_inplace_by() should just sort the slice.
 const SORT_THRESH: usize = 6;
 
+pub fn median_inplace_by<T, F: FnMut(&T, &T) -> Ordering>(data: &mut [T], ord_fn: F) -> &mut T {
+    if data.len() < 3 { return &mut data[0]; }
+
+    let median_idx = data.len() / 2;
+
+    qselect_inplace_by(data, median_idx, ord_fn)
+}
+
 /// Use the Quickselect algorithm, given an ordering function,
 /// to select the `k`th smallest element from `data`. As part of the
 /// algorithm, `k` is moved to its final sorted position and the rest of the array is (at least) partially
@@ -13,7 +21,7 @@ const SORT_THRESH: usize = 6;
 ///
 /// ##Panics
 /// If `k` is greater than `data.len()`.
-pub fn qselect_inplace_by<T, F: FnMut(&T, &T) -> Ordering>(data: &mut [T], k: usize, mut ord_fn: F) -> &mut T {
+fn qselect_inplace_by<T, F: FnMut(&T, &T) -> Ordering>(data: &mut [T], k: usize, mut ord_fn: F) -> &mut T {
     let len = data.len();
 
     assert!(k < len, "Called qselect_inplace with k = {} and data length: {}", k, len);
@@ -34,6 +42,27 @@ pub fn qselect_inplace_by<T, F: FnMut(&T, &T) -> Ordering>(data: &mut [T], k: us
     }
 }
 
+fn partition_around<T, F>(data: &mut [T], pivot_idx: usize, mut ord_fn: F) -> usize
+    where F: FnMut(&T, &T) -> Ordering {
+
+    let len = data.len();
+
+    data.swap(pivot_idx, len - 1);
+
+    let mut curr = 0;
+
+    for i in 0..len - 1 {
+        if ord_fn(&data[i], &data[len - 1]) == Ordering::Less {
+            data.swap(i, curr);
+            curr += 1;
+        }
+    }
+
+    data.swap(curr, len - 1);
+
+    curr
+}
+
 /// Given an ordering function, pick an arbitrary pivot and partition the slice, returning the pivot.
 fn partition_by<T, F: FnMut(&T, &T) -> Ordering>(data: &mut [T], mut ord_fn: F) -> usize {
     let len = data.len();
@@ -46,25 +75,7 @@ fn partition_by<T, F: FnMut(&T, &T) -> Ordering>(data: &mut [T], mut ord_fn: F) 
         median_of_3_by(&first, &mid, &last, |left, right| ord_fn(left.0, right.0)).1
     };
 
-    data.swap(pivot_idx, len - 1);
-
-    let mut curr = 0;
-
-    for i in 0 .. len - 1 {
-        if ord_fn(&data[i], &data[len - 1]) == Ordering::Less {
-            data.swap(i, curr);
-            curr += 1;
-        }
-    }
-
-    data.swap(curr, len - 1);
-
-    curr
-}
-
-/// Of the three values passed, return the median.
-pub fn median_of_3<T: Ord>(x: T, y: T, z: T) -> T {
-    median_of_3_by(x, y, z, <T as Ord>::cmp)
+    partition_around(data, pivot_idx, ord_fn)
 }
 
 /// Given an ordering function, of the three values passed, return the median.
@@ -76,53 +87,10 @@ pub fn median_of_3_by<T, F: FnMut(&T, &T) -> Ordering>(mut x: T, mut y: T, mut z
     y
 }
 
-/// If `x > y`, swap `x` and `y`.
-#[inline]
-pub fn in_order<T: Ord>(x: &mut T, y: &mut T) {
-    if x > y {
-        mem::swap(x, y);
-    }
-}
-
 /// Given an ordering function, if `x > y`, swap `x` and `y`.
 #[inline]
 pub fn in_order_by<T, F: FnMut(&T, &T) -> Ordering>(x: &mut T, y: &mut T, mut ord_fn: F) {
     if ord_fn(x, y) == Ordering::Greater {
         mem::swap(x, y);
     }
-}
-
-#[test]
-fn test_qsel() {
-    macro_rules! test_qsel (
-        ($($elem:expr),+; $k:expr; $expect:expr) => (
-            let mut data = [$($elem),+];
-            let selected = qselect_inplace(&mut data, $k);
-            assert_eq!(*selected, $expect);
-        )
-    );
-
-    test_qsel!(1, 2, 3, 4, 5; 3; 4);
-    test_qsel!(5, 4, 3, 2, 1; 1; 2);
-    test_qsel!(2, 2, 3, 3, 4; 1; 2);
-}
-
-#[test]
-fn test_median() {
-    macro_rules! test_median {
-        ($x:expr, $y:expr, $z:expr; $expected:expr) => (
-            let x = $x;
-            let y = $y;
-            let z = $z;
-            let result = median_of_3(&x, &y, &z);
-            assert_eq!(*result, $expected);
-        )
-    }
-    
-    test_median!(1, 2, 3; 2);
-    test_median!(1, 3, 2; 2);
-    test_median!(2, 1, 3; 2);
-    test_median!(2, 3, 1; 2);
-    test_median!(3, 1, 2; 2);
-    test_median!(3, 2, 1; 2);
 }
