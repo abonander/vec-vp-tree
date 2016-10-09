28015fa6dbfd1d3de754f30afcb2ff42df1e2b76
Austin Bonander <austin.bonander@gmail.com>
2016-10-08 23:27:04 -0700

    Principal implementation finished

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,29 @@
 extern crate rand;
+extern crate smallvec;
 
 use rand::Rng;
+
+use smallvec::SmallVec;
+
+use std::cmp::Ordering;
+use std::collections::BinaryHeap;
 
 mod select;
 
 const NO_NODE: usize = ::std::usize::MAX;
 
-pub struct VpTree<T, Df> {
+pub struct VpTree<T, D> {
     nodes: Vec<Node>,
     items: Vec<T>,
-    dist_fn: Df,
+    dist_fn: D,
 }
 
-impl<T, Df> VpTree<T, Df> where Df: FnMut(&T, &T) -> u64 {
-    pub fn new<I: IntoIterator<Item = T>>(items: I, dist_fn: Df) -> Self {
+impl<T: Eq, D: VpDist<T>> VpTree<T, D> {
+    pub fn new<I: IntoIterator<Item = T>>(items: I, dist_fn: D) -> Self {
         Self::from_vec(items.into_iter().collect(), dist_fn)
     }
 
-    pub fn from_vec(items: Vec<T>, dist_fn: Df) -> Self {
+    pub fn from_vec(items: Vec<T>, dist_fn: D) -> Self {
         let mut self_ = VpTree {
             nodes: Vec::with_capacity(items.len()),
             items: items,
@@ -57,16 +63,16 @@ impl<T, Df> VpTree<T, Df> where Df: FnMut(&T, &T) -> u64 {
             let (pivot, items) = self.items.split_first_mut().unwrap();
 
             // Without this reborrow, the closure will try to borrow all of `self`.
-            let mut dist_fn = &mut self.dist_fn;
+            let dist_fn = &self.dist_fn;
 
             // This function will partition around the median element so we don't need
             // as second partial-sort run
             let median_thresh_item = select::median_inplace_by(
                 items,
-                |left, right| dist_fn(pivot, left).cmp(&dist_fn(pivot, right))
+                |left, right| dist_fn.dist(pivot, left).cmp(&dist_fn.dist(pivot, right))
             );
 
-            dist_fn(pivot, median_thresh_item)
+            dist_fn.dist(pivot, median_thresh_item)
         };
 
         let self_idx = self.push_node(parent_idx, start, threshold);
@@ -101,8 +107,7 @@ impl<T, Df> VpTree<T, Df> where Df: FnMut(&T, &T) -> u64 {
     }
 
     /// Apply a new distance function and rebuild the tree, returning the transformed type.
-    pub fn dist_fn<Df_>(self, dist_fn: Df_) -> VpTree<T, Df_>
-        where Df_: FnMut(&T, &T) -> u64 {
+    pub fn dist_fn<D_: VpDist<T>>(self, dist_fn: D_) -> VpTree<T, D_> {
         let mut self_ = VpTree {
             nodes: self.nodes,
             items: self.items,
@@ -118,14 +123,30 @@ impl<T, Df> VpTree<T, Df> where Df: FnMut(&T, &T) -> u64 {
         &self.items
     }
 
-    pub fn neighbors<'t, 'o>(&'t self, origin: &'o T, radius: u64) -> Neighbors<'t, 'o, T, Df>
-    where T: Eq {
+    pub fn neighbors<'t, 'o>(&'t self, origin: &'o T, radius: u64) -> Neighbors<'t, 'o, T, D> {
         Neighbors {
             tree: self,
             origin: origin,
+            splits: SmallVec::new(),
             current_node: if self.nodes.len() > 0 { 0 } else { NO_NODE },
-            radius: radius
+            radius: radius,
         }
+    }
+
+    pub fn k_nearest<'t, 'o>(&'t self, origin: &'o T, k: usize) -> Vec<Neighbor<'t, T>> {
+        let mut heap = BinaryHeap::new();
+
+        let mut neighbors = self.neighbors(origin, ::std::u64::MAX);
+
+        while let Some(neighbor) = neighbors.next() {
+            heap.push(neighbor);
+
+            if heap.len() == k {
+                neighbors.radius = heap.pop().unwrap().dist;
+            }
+        }
+
+        heap.into_sorted_vec()
     }
 }
 
@@ -137,36 +158,116 @@ struct Node {
     threshold: u64,
 }
 
-pub struct Neighbors<'t, 'o, T: 't + 'o, Df: 't> {
-    tree: &'t VpTree<T, Df>,
+pub struct Neighbors<'t, 'o, T: 't + 'o, D: 't> {
+    tree: &'t VpTree<T, D>,
     origin: &'o T,
+    splits: SmallVec<[usize; 3]>,
     current_node: usize,
     radius: u64,
 }
 
-impl<'t, 'o, T: 't + 'o, Df: 't> Iterator for Neighbors<'t, 'o, T, Df>
-where Df: Fn(&T, &T) -> u64, T: Eq {
-    type Item = (&'t T, u64);
+impl<'t, 'o, T: 't + 'o, D: 't> Iterator for Neighbors<'t, 'o, T, D> where T: Eq, D: VpDist<T> {
+    type Item = Neighbor<'t, T>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.current_node == NO_NODE {
-            return None;
-        }
+        let mut ret = None;
 
-        if self.tree.nodes.len() == 0 { return None; }
+        while let None = ret {
+            if self.current_node == NO_NODE {
+                if let Some(&last_split) = self.splits.last() {
+                    self.current_node = last_split;
+                } else {
+                    break;
+                }
+            }
 
-        let cur_node = &self.tree.nodes[self.current_node];
+            let cur_node = &self.tree.nodes[self.current_node];
 
-        let dist_to_cur = (self.tree.dist_fn)(&self.origin, &self.tree.items[cur_node.idx]);
+            let dist_to_cur = self.tree.dist_fn.dist(&self.origin, &self.tree.items[cur_node.idx]);
 
-        if dist_to_cur <= cur_node.threshold {
-            if cur_node.left == NO_NODE {
+            let go_left = dist_to_cur.saturating_sub(self.radius) <= cur_node.threshold;
+            let go_right = dist_to_cur.saturating_add(self.radius) >= cur_node.threshold;
 
+            let already_seen = if let Some(last_split) = self.splits.pop() {
+                let already_seen = self.current_node == last_split;
+
+                if !already_seen {
+                    self.splits.push(last_split);
+                }
+
+                already_seen
+            } else {
+                false
+            };
+
+            if dist_to_cur <= cur_node.threshold {
+                if go_left && !already_seen {
+                    if go_right {
+                        self.splits.push(self.current_node);
+                    }
+
+                    self.current_node = cur_node.left;
+                }
+
+                if go_right {
+                    self.current_node = cur_node.right;
+                }
+            } else {
+                if go_right && !already_seen {
+                    if go_left {
+                        self.splits.push(self.current_node);
+                    }
+
+                    self.current_node = cur_node.right;
+                }
+            }
+
+            if dist_to_cur <= self.radius {
+                ret = Some(Neighbor {
+                    item: &self.tree.items[cur_node.idx],
+                    dist: dist_to_cur
+                });
             }
         }
 
-        if  dist_to_cur <= self.radius {
+        ret
+    }
+}
 
-        }
+/// Wrapper of an item and a distance, returned by `Neighbors`.
+pub struct Neighbor<'t, T: 't> {
+    pub item: &'t T,
+    pub dist: u64,
+}
+impl<'t, T: 't> PartialOrd for Neighbor<'t, T> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+
+/// Returns the comparison of the distances only
+impl<'t, T: 't> Ord for Neighbor<'t, T> {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.dist.cmp(&other.dist)
+    }
+}
+
+/// Returns the equality of the distances only.
+impl<'t, T: 't> PartialEq for Neighbor<'t, T> {
+    fn eq(&self, other: &Self) -> bool {
+        self.dist == other.dist
+    }
+}
+
+impl<'t, T: 't> Eq for Neighbor<'t, T> {}
+
+pub trait VpDist<T> where T: Eq {
+    fn dist(&self, left: &T, right: &T) -> u64;
+}
+
+impl<T, F> VpDist<T> for F where T: Eq, F: Fn(&T, &T) -> u64 {
+    fn dist(&self, left: &T, right: &T) -> u64 {
+        (self)(left, right)
     }
 }
