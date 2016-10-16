091407fb1ccf3ca67a234e613ee8090683f72ed7
Austin Bonander <austin.bonander@gmail.com>
2016-10-15 20:36:57 -0700

    rustfmt made this doc comment into a regular comment, bad rustfmt

--- src/dist/string.rs
+++ src/dist/string.rs
@@ -4,8 +4,8 @@
 // http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
 // http://opensource.org/licenses/MIT>, at your option. This file may not be
 // copied, modified, or distributed except according to those terms.
-// Distance functions for strings (`strsim` feature, enabled by default)
 
+//! Distance functions for strings (`strsim` feature, enabled by default)
 use strsim;
 
 use super::{DistFn, KnownDist};
