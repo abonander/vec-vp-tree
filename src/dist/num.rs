39de187d10e67e064da41049850e282399a7a423
Austin Bonander <austin.bonander@gmail.com>
2016-10-15 20:38:13 -0700

    rustfmt made this doc comment into a regular comment, bad rustfmt (2)

--- src/dist/num.rs
+++ src/dist/num.rs
@@ -4,7 +4,8 @@
 // http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
 // http://opensource.org/licenses/MIT>, at your option. This file may not be
 // copied, modified, or distributed except according to those terms.
-// Distance functions for numeric types.
+
+//! Distance functions for numeric types.
 
 use super::{DistFn, KnownDist};
 
